3c17d8d1c1335946dc9da90da1cf5172ca77860c
Sameer Memon <memonsameer4@gmail.com>
2020-08-24 18:00:46 +0530

    Update admin.py

--- clashRC/admin.py
+++ clashRC/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Profile, Question, Submissions
+myModels = [Profile, Question, Submissions]
+admin.site.register(myModels)
 
-# Register your models here.
